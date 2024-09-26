73a4f7c4568611e298e21bf7773a8b95b8e194ce
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-09-27 02:13:48 +0900

    init: eslint, prettier기초설설정

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  overrides: [],
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'type'],
+        pathGroups: [
+          {
+            pattern: 'react+(|-native)',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+};
