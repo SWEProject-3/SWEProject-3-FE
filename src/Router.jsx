e4f0de449684860d8810ad9c1f17b73b06deb821
applepops <jihee336723@gmail.com>
2024-10-18 01:43:52 +0900

    feat:mypage와 정보 수정 페이지 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '@/pages/home';
 import SplashRouter from './router/SplashRouter';
 import CalendarPage from '@/pages/calendar';
+import MyPage from '@/pages/mypage';
+import EditProfilePage from '@/pages/editprofile';
 
 export default function AppRouter() {
   return (
@@ -14,8 +16,9 @@ export default function AppRouter() {
         <Route path='/calendar' element={<CalendarPage />} />
         <Route path='/feed' element={<Home />} />
         <Route path='/search' element={<Home />} />
-        <Route path='/my' element={<Home />} />
+        <Route path='/my' element={<MyPage />} />
         <Route path='/alarm' element={<Home />} />
+        <Route path='/edit-profile' element={<EditProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
