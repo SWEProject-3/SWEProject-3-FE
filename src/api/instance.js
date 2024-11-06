aff3a289745f8c31a9124c50a83ccf346bcc24d4
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-11-07 05:52:44 +0900

    fix: 로그인 코드 수정

--- src/api/instance.js
+++ src/api/instance.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
+    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
   },
 });
 
