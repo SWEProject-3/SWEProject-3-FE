e8f19d8c07c140fcbba987797e0349e35398bbfb
choi <joeplay0801@gmail.com>
2024-11-28 11:26:42 +0900

    feat: interceptor구현 (accessToken이 header에 가끔 적용안되는 이유)

--- src/api/instance.js
+++ src/api/instance.js
@@ -3,9 +3,19 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    // 'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+    'Content-Type': 'application/json',
+    // Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
   },
 });
 
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 export default instance;
